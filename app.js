0cddecbc610306cd8274cc1673011f091cdb7e82
avaldivia97 <68609260+avaldivia97@users.noreply.github.com>
2020-12-03 13:39:57 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ let scoreText = document.getElementById("score");
 let clicksPerSecondText = document.getElementById("CPS");
 let startButton = document.getElementById("startButton");
 let clickArea = document.getElementById("clickArea");
+let oneSecond = document.getElementById("1s");
+let fiveSeconds = document.getElementById("5s");
+let tenSeconds = document.getElementById("10s");
+let thirtySeconds = document.getElementById("30s");
+let sixtySeconds = document.getElementById("60s");
+changeStatsText(gameLength);
+
+
 
 function showElement(element){
     element.style.display = 'inline';
@@ -15,6 +23,35 @@ function showElement(element){
 
 function hideElement(element){
     element.style.display = 'none';
+}
+
+function changeGameLength(){
+    if (oneSecond.checked){
+        gameLength = 1;
+        changeStatsText(gameLength);
+    }
+    if (fiveSeconds.checked){
+        gameLength = 5;
+        changeStatsText(gameLength);
+    }
+    if (tenSeconds.checked){
+        gameLength = 10;
+        changeStatsText(gameLength);
+    }
+    if (thirtySeconds.checked){
+        gameLength = 30;
+        changeStatsText(gameLength);
+    }
+    if (sixtySeconds.checked){
+        gameLength = 60;
+        changeStatsText(gameLength);
+    }
+}
+
+function changeStatsText(time){
+    timerText.innerText = time;
+    scoreText.innerText = 0;
+    clicksPerSecondText = 0;
 }
 
 function startGame(){
@@ -26,7 +63,7 @@ function startGame(){
     let timer = setInterval(() => {
         let timePassed = (new Date().getTime() - startTime) / 1000;
         if(timePassed < gameLength){
-            timerText.innerText = timePassed.toFixed(3);
+            timerText.innerText = (gameLength - timePassed).toFixed(3);
             clicksPerSecondText.textContent = (score/timePassed).toFixed(2);
         }
         else{
@@ -38,7 +75,7 @@ function startGame(){
 }
 
 function endGame(){
-    timerText.innerText = gameLength.toFixed(3);
+    timerText.innerText = 0;
     score.textContent = score;
     clicksPerSecond = (score/gameLength).toFixed(2);
     clicksPerSecondText.textContent = clicksPerSecond;
